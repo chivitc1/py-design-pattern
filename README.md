34504ff417251bdaf536293a87beb00f80ec6e3f
nguyen vinh chi <chinv@nguyens-Mac-mini.local>
2018-07-19 16:44:13 +0700

    Auth example

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
 # py-design-pattern
+
+## Set up python3 venv
+$ cd [project_dir]
+
+$ python3 -m pip install virtualenv
+
+$ python3 -m virtualenv venv
+
+$ source venv/bin/activate
---
+++ auth/user.py
@@ -0,0 +1,47 @@
+import hashlib
+
+class User:
+    def __init__(self, username, password):
+        '''Create a new user object. The password
+        will be encrypted before storing.'''
+        self.username = username
+        self.password = self._encrypt_pass(password)
+        self.is_logged_in = False
+
+    def _encrypt_pass(self, password):
+        '''Encrypt the password with the username and return
+        the sha digest.'''
+        hash_string = self.username + self.password
+        hash_string = hash_string.encode("utf8")
+        return hashlib.sha256(hash_string).hexdigest()
+
+    def check_password(self, password):
+        '''Return True if the password is valid for this
+        user, false otherwise.'''
+        encrypted = self._encrypt_pass(password)
+        return encrypted == self.password
+
+class AuthException(Exception):
+    def __init__(self, username, user):
+        super().__init__(username, user)
+        self.username = username
+        self.user = user
+
+class UsernameAlreadyExists(AuthException):
+    pass
+
+class PasswordTooShort(AuthException):
+    pass
+
+class Authenticator:
+    def __init__(self):
+        '''Construct an authenticator to manage
+        users logging in and out.'''
+        self.users = {}
+
+    def add_user(self, username, password):
+        if username in self.users:
+            raise UsernameAlreadyExists(username)
+        if len(password) < 6:
+            raise PasswordTooShort(username)
+        self.users[username] = User(username, password)
